3b3ef20212cb9d4ddd4fbee03796bbc5d8e0b3d9
Tetsuro Mikami <tetsuro@mickamy.com>
2026-07-21 15:24:50 +0900

    feat: add server skeleton with request validation

---
+++ cmd/server/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"cmp"
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/mickamy/employee-management/internal/server"
+)
+
+func main() {
+	if err := run(); err != nil {
+		slog.Error("server exited with error", "error", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	srv := server.New(":" + cmp.Or(os.Getenv("PORT"), "8080"))
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	errCh := make(chan error, 1)
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			errCh <- fmt.Errorf("listen and serve: %w", err)
+		}
+	}()
+
+	slog.Info("server listening", "addr", srv.Addr)
+
+	select {
+	case err := <-errCh:
+		return err
+	case <-ctx.Done():
+	}
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		return fmt.Errorf("shutdown: %w", err)
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,23 @@
 module github.com/mickamy/employee-management
 
 go 1.26.5
+
+require (
+	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.36.11-20260709200747-435963d16310.1
+	connectrpc.com/connect v1.20.0
+	connectrpc.com/validate v0.6.0
+	google.golang.org/genproto v0.0.0-20260720211330-0afa2a65878a
+	google.golang.org/protobuf v1.36.11
+)
+
+require (
+	buf.build/go/protovalidate v1.0.0 // indirect
+	cel.dev/expr v0.24.0 // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.1 // indirect
+	github.com/google/cel-go v0.26.1 // indirect
+	github.com/stoewer/go-strcase v1.3.1 // indirect
+	golang.org/x/exp v0.0.0-20250911091902-df9299821621 // indirect
+	golang.org/x/text v0.40.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20260720155508-bb71a54f79dc // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260720155508-bb71a54f79dc // indirect
+)
